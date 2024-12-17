6f8291822707ce5132ec4043dd9e61a56e6347d6
Martin <martin_lenart@icloud.com>
2024-12-17 14:42:45 +0100

    update

--- 02_Tasks/Program.cs
+++ 02_Tasks/Program.cs
@@ -4,25 +4,28 @@ using Seido.Utilities.SeedGenerator;
 
 Console.WriteLine("Hello, World!");
 var seeder = new SeedGenerator();
-Task<int> t1=null;
-Task<int> t2=null; 
-Task<int> t3=null;
+
+List<Task<int>> tasks = new List<Task<int>>();
 try
 {
-    t1 = WriteNamesAsync(seeder, 5);
-    t2 = WriteLatinAsync(seeder, 10);
-    t3 = WriteMusicAsync(seeder, 20);
+    tasks.Add(WriteNamesAsync(seeder, 5));
+    tasks.Add(WriteLatinAsync(seeder, 10));
+    tasks.Add(WriteMusicAsync(seeder, 20));
 
-    Task.WaitAll(t1, t2, t3);
-    Console.WriteLine($"\nt1 result: {t1.Result}, t2 result: {t2.Result}, t3 result: {t3.Result}");
+    Task.WaitAll(tasks[0], tasks[1], tasks[2]);
+    foreach (var item in tasks)
+    {
+        Console.WriteLine($"\ntask result: {item.Result}");
+    }
 }
 catch (Exception ex)
 {
     System.Console.WriteLine($"{ex.Message}");
 
-    System.Console.WriteLine($"t1 status {t1?.Status}");
-    System.Console.WriteLine($"t2 status {t2?.Status}");
-    System.Console.WriteLine($"t3 status {t3?.Status}");
+    foreach (var item in tasks)
+    {
+        Console.WriteLine($"\ntask status: {item.Status}");
+    }
 }
 
 System.Console.WriteLine("Main terminated.");
@@ -36,7 +39,6 @@ static int WriteNames(SeedGenerator seeder, int nrItems)
     int sum = 0;
     for (int i = 0; i < nrItems; i++)
     {
-        if (i == 2) throw new ArgumentException("Exception in WriteNames");
 
         System.Console.WriteLine($"{seeder.FullName} - {i}"); 
         Task.Delay(50).Wait();  
@@ -64,6 +66,7 @@ static int WriteMusic(SeedGenerator seeder, int nrItems)
     int sum = 0;
     for (int i = 0; i < nrItems; i++)
     {
+        if (i == 2) throw new ArgumentException("Exception in WriteNames");
         System.Console.WriteLine($"{seeder.MusicGroupName} - {i}");        
         Task.Delay(50).Wait();   
         sum += i;      
