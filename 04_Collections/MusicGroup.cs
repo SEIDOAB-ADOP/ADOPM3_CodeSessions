56e94568a5857b105c2d99e816c20d1b72713a04
Martin <martin_lenart@icloud.com>
2025-12-17 12:13:46 +0100

    update

---
+++ 04_Collections/MusicGroup.cs
@@ -0,0 +1,40 @@
+﻿using Seido.Utilities.SeedGenerator;
+
+namespace Models.Music;
+
+public enum MusicGenre {Rock, Blues, Jazz, Metal}
+public class MusicGroup : ISeed<MusicGroup>
+{
+    public virtual Guid MusicGroupId { get; set; }
+    public virtual string Name { get; set; }
+    public virtual int EstablishedYear { get; set; }
+
+    public virtual MusicGenre Genre { get; set; }
+ 
+    #region Constructors
+    public MusicGroup(){}
+    public MusicGroup(MusicGroup org)
+    {
+        MusicGroupId = org.MusicGroupId;
+        Name = org.Name;
+        EstablishedYear = org.EstablishedYear;
+        Genre = org.Genre;
+    }
+    #endregion
+
+    #region randomly seed this instance
+    public virtual bool Seeded { get; set; } = false;
+    public virtual MusicGroup Seed(SeedGenerator seedGenerator)
+    {
+        Seeded = true;
+        MusicGroupId = Guid.NewGuid();
+
+        Name = seedGenerator.MusicGroupName;
+        EstablishedYear = seedGenerator.Next(1970, 2024);
+        Genre = seedGenerator.FromEnum<MusicGenre>();
+        return this;
+    }
+    #endregion
+}
+
+
---
+++ 04_Collections/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Models.Music;
+using Seido.Utilities.SeedGenerator;
+
+Console.WriteLine("Hello, Collections!");
+
+var seeder = new SeedGenerator();
+
+//List<T>
+List<MusicGroup> music = seeder.ItemsToList<MusicGroup>(10);
+
+music.ForEach(Console.WriteLine);
